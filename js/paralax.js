b7743205696b34709819f0438e6cc73036230f42
Meff <meffmara@gmail.com>
2016-11-07 12:39:51 +0200

    'loadProject'

---
+++ js/paralax.js
@@ -0,0 +1,34 @@
+// Paralax plagin for site BeFlex
+// Step 1
+
+$(window).scroll(function(event) {
+	
+	var st = $(this).scrollTop();
+
+	// $('header').css({
+	// 	"transform" : "translate(0%, -" + st/4 + "%"
+	// });
+
+
+ // $('.intro-heading').css({
+	// 	"transform" : "translate(0%, " + st/4 + "%"
+	// 	});
+
+
+ // $('#buton').css({
+	// 	"transform" : "translate(0%, " + st/3 + "%"
+	// 	});
+
+
+ // $('.section-heading').css({
+ // 		"transform":" translate(0%, -" + st/3 +"%"
+ // });
+
+ // $('.section-subheading text-muted').css({
+ // 		"transform":" translate(0%, -" + st/3 +"%"
+ // });
+
+
+
+
+});
